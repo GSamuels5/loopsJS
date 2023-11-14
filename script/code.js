d25f7c4ce9898abcfb8ada665c22356fcabb3b30
GSamuels5 <ghamzahsamuels23@gmail.com>
2023-11-14 16:41:20 +0200

    middle added

---
+++ script/code.js
@@ -0,0 +1,9 @@
+let numbers = [12, 18, 11, 5, 8, 2];
+
+let middleNumber = Math.floor(numbers.length/2)
+
+
+if (numbers.length % 2 == 0) {
+    console.log('middle numbers:', numbers[middleNumber -1], numbers[middleNumber]);
+    
+} 
