51d3365676eea72406889b760730c7c92a03c51c
GSamuels5 <ghamzahsamuels23@gmail.com>
2023-11-14 16:53:59 +0200

    changes

--- script/code.js
+++ script/code.js
@@ -1,9 +1,4 @@
 let numbers = [12, 18, 11, 5, 8, 2];
 
-let middleNumber = Math.floor(numbers.length/2)
-
-
-if (numbers.length % 2 == 0) {
-    console.log('middle numbers:', numbers[middleNumber -1], numbers[middleNumber]);
-    
-} 
+let middleNumber = numbers.slice(2, 4)
+console.log(middleNumber);
